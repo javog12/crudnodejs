319e3b13896db2dea5b0a3abd331a021fdbf62f7
Chavichenko <javig.aguilar12@gmail.com>
2020-11-06 22:55:15 +0000

    using module path to route html files

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 //settings
 app.set('port',4000);
@@ -8,7 +9,7 @@ app.set('port',4000);
 // routes
 
 app.get('/',(req,res) => {
-	res.send('hello world');
+	res.sendFile(path.join(__dirname,'views/index.html'));
 });
 
 // static files
